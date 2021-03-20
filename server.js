f95e1aeec836cfbf6a8813fb1fc862a9926174a7
AlbertTundy <76978834+AlbertTundy@users.noreply.github.com>
2021-03-20 15:16:00 -0600

    added server.js

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const logger = require("morgan");
+const apiRoutes = require("./routes/apiRoutes");
+const htmlRoutes = require("./routes/htmlRoutes");
+const PORT = 3000;
+
+const app = express();
+app.use(logger("dev"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
+mongoose.connect(
+  process.env.MONGODB_URI || "mongodb://localhost/Workout",
+{
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: false
+});
+app.use(require("./routes/apiRoutes"));
+app.use(require("./routes/htmlRoutes"));
+
+app.listen(PORT, () => {
+  console.log(`App running on port http://localhost:${PORT}`);
+}); 
