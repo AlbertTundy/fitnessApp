5dcec14909f38c81d503771d893f4a21273e8eb4
AlbertTundy <76978834+AlbertTundy@users.noreply.github.com>
2021-03-20 18:38:20 -0600

    commented out some stuff for science

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const logger = require("morgan");
-const apiRoutes = require("./routes/apiRoutes");
-const htmlRoutes = require("./routes/htmlRoutes");
+// const apiRoutes = require("./routes/apiRoutes");
+// const htmlRoutes = require("./routes/htmlRoutes");
 const PORT = process.env.PORT || 8080;
 
 const app = express();
